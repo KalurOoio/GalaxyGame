9f2cef82eefbd07fbcd2df6af2eff1a1393b44a1
XDMiFy <r.bakman@icloud.com>
2022-12-25 16:47:13 +0600

    Init

---
+++ Assets/Scripts/HelpersScripts/BackgroundHelper.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundHelper : MonoBehaviour
+{
+    public Renderer backRenderer;
+    void Update()
+    {
+        if (backRenderer!= null) {
+            backRenderer.material.mainTextureOffset = new Vector2(0.0f, 0.1f * Time.time);
+        }
+    }
+}
